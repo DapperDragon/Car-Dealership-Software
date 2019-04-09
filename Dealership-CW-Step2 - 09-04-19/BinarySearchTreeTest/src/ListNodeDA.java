437958ab566327f752e104b9fe673b79939e305a
Gary H <gman.gary@hotmail.co.uk>
2019-04-09 16:01:25 +0100

    Add files via upload
    show cars by specific manufacturer and manufacturer/model methods added

---
+++ Dealership-CW-Step2 - 09-04-19/BinarySearchTreeTest/src/ListNodeDA.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author B00330262
+ */
+public class ListNode {
+    
+    
+ 
+
+    public Comparable object;
+    public ListNode next;
+    
+    
+}
